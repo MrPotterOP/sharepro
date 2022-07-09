8d29186ef27af072211792df8984163e370f0bf1
Shubham Ubarhande <shubham.ubarhande1044@gmail.com>
2022-07-09 16:08:05 +0530

    added-how-it-works-section

--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -3,12 +3,14 @@ import React from "react";
 //components
 import Navbar from "../Navbar";
 import Hero from "../Hero";
+import WorkSection from "../WorkSection";
 
 const LandingPage = ()=>{
     return(
         <section className="landing-page">
             <Navbar />
             <Hero />
+            <WorkSection />
         </section>
     )
 };
