0bec8d1fe8bdd15d965acf8b93c9037b01875aa5
Shubham Ubarhande <shubham.ubarhande1044@gmail.com>
2022-07-11 14:44:32 +0530

    created bsic platform route file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,14 @@ import {Route, BrowserRouter, Routes} from "react-router-dom";
 
 //Components
 import LandingPage from "./components/LandingPage/LandingPage";
+import Platform from "./components/Platform/Platform";
 
 const App = ()=>{
     return (
        <BrowserRouter>
             <Routes>
                 <Route path="/" element={<LandingPage />} exact />
-                  
+                <Route path="/platform" element={<Platform />} exact />
             </Routes>
        </BrowserRouter>
     )
---
+++ frontend/src/components/Platform/Platform.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+//          Components
+import Navbar from '../Navbar';
+
+const Platform = ()=>{
+    return(
+        <Navbar />
+    );
+};
+
+export default Platform;;
