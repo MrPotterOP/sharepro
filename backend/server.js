9024deb5ed4e8eb4c99bedfdc883d25160867761
Shubham Ubarhande <shubham.ubarhande1044@gmail.com>
2022-07-14 15:18:14 +0530

    connection to mongodb through backend established

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
 import express from "express";
+import dotenv from "dotenv";
 import mongoose from "mongoose";
 
+dotenv.config();
+
+mongoose.connect(process.env.MONGOURL, {useNewUrlParser: true, useUnifiedTopology: true}, (err)=>{
+    if(!err){
+        console.log("Database Connected.");
+    }else{
+        console.log(err);
+    }
+});
 
 const App = express();
 
 App.use(express.json());
 
 
-App.listen(4000, (req, res)=>{
+App.listen(process.env.PORT || 4000, (req, res)=>{
     console.log("Backend Server is Live...");
 });
